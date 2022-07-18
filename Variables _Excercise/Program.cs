403f2676111ab4e96530423859cf2c7b13298779
forukotan <forukotan1@gmail.com>
2022-07-18 13:01:27 -0500

    initial commit

---
+++ Variables _Excercise/Program.cs
@@ -0,0 +1,8 @@
+﻿
+        string MyName = "Femi Orukotan";
+    int MyAge = 26;
+    char at = '@';
+bool OlderThan25 = true;
+double a = 2;
+decimal b = 1.34m;
+Console.WriteLine($"Hi my name is {MyName}, I'm {MyAge}and i live {at} 1234 pine tree lane. I have {a} siblings ");
